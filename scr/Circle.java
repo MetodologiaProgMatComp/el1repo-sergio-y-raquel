c3a7aca991f96b96067bf3eb3c35ddfa603660fd
sergioruizj <sergio.ruizj@edu.uah.es>
2024-02-07 09:09:37 +0100

    Prueba

---
+++ scr/Circle.java
@@ -0,0 +1,14 @@
+/***
+ * The Circle Class models a circle with a radius and color
+ */
+
+public class Circle {
+    // private instance variable, not accessible from outside this class
+    private double radius;
+    private String color;
+    // Constructors (overloaded)
+    // Constructs a Circle instance with default value for radius and color */
+
+
+
+}
