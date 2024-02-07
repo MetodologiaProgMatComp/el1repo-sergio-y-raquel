da49f43d63d2ce6a232b0b0e9460c261a6dabdc5
sergioruizj <sergio.ruizj@edu.uah.es>
2024-02-07 09:36:51 +0100

    Prueba

--- scr/Circle.java
+++ scr/Circle.java
@@ -11,7 +11,7 @@ public class Circle {
     // Constructs a Circle instance with default value for radius and color */
     public Circle() {
         radius = 1.0;
-        color = "blue";
+        color = "green";
     }
 
 
