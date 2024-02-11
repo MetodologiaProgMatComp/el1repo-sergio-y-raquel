2b5e9dfe3844164004eeeb6aaeaa9f36650dd5b2
sergioruizj <sergio.ruizj@edu.uah.es>
2024-02-11 19:29:05 +0100

    11/02 19:28

---
+++ scr/es/uah/matcomp/mp/e1/ejerciciosclases/Employee.java
@@ -0,0 +1,51 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Employee {
+    int id;
+    String firstName;
+    String lastName;
+    int salary;
+
+    public void employee(int id, String FN, String LN, int Sal) {
+        this.firstName = FN;
+        this.id = id;
+        this.lastName = LN;
+        this.salary = Sal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public String getName(){
+        return firstName+" "+lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int getAnnualSalary(){
+        return salary*12;
+    }
+    public double raiseSalary(double percent){
+        return salary*(percent/100)+salary;
+    }
+
+    /***
+     * Hemos cambiado el Main porque no funcionaba al tener que trabajar con datos decimales
+     */
+    public String toString() {
+        return "Employee[id= " + id +", name= "+firstName+" "+lastName+", salary= "+salary+"]";
+    }
+}
---
+++ scr/es/uah/matcomp/mp/e1/ejerciciosclases/Rectangle.java
@@ -0,0 +1,41 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Rectangle {
+    float lenght=1.0f;
+    float width=1.0f;
+
+    public void rectangle(){
+        this.lenght=1.0f;
+        this.width=1.0f;
+    }
+
+    public void rectangle(float len,float wid){
+        this.lenght=len;
+        this.width=wid;
+    }
+
+    public float getLength() {
+        return lenght;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setLength(float lenght) {
+        this.lenght = lenght;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+    public double getArea(){
+        return lenght*width;
+    }
+    public double getPerimeter(){
+        return 2*lenght+2*width;
+    }
+    public String toString() {
+        return "Rectangle[length= " + lenght +", width "+width+"]";
+    }
+}
