9ce057f0e5c064f3f78ea067631b7546748316ca
sergioruizj <sergio.ruizj@edu.uah.es>
2024-02-11 21:29:25 +0100

    Unversioned Files

---
+++ Test/Java/es/uah/matcomp/mp/e1/ejerciciosclases/EmployeeTest.java
@@ -0,0 +1,69 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @org.junit.jupiter.api.Test
+    void employee() {
+        Employee em  = new Employee();
+        assertDoesNotThrow(()->em.employee(1,"Juan","Rodriguez",300));
+    }
+
+    @org.junit.jupiter.api.Test
+    void getId() {
+        Employee em  = new Employee();
+        int id = em.getId();
+        assertNotEquals(id, 0);
+        assertTrue((id>0),"Error, es menor o igual a 0");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirstName() {
+        /***
+         * Preguntar al profesor que que "assert" hay que usar
+         */
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSalary() {
+        Employee em  = new Employee();
+        int sal = em.getSalary();
+        assertNotEquals(sal, 0);
+        assertTrue((sal>0),"Error, es menor o igual a 0");
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSalary() {
+        Employee em  = new Employee();
+        assertDoesNotThrow(()->em.setSalary(300));
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAnnualSalary() {
+        Employee em  = new Employee();
+        int sal = em.getAnnualSalary();
+        assertNotEquals(sal, 0);
+        assertTrue((sal>0),"Error, es menor o igual a 0");
+    }
+
+    @org.junit.jupiter.api.Test
+    void raiseSalary() {
+        Employee em  = new Employee();
+        double sal = em.raiseSalary(10.7);
+        assertNotEquals(sal, 0.0);
+        assertTrue((sal>0),"Error, es menor o igual a 0");
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
