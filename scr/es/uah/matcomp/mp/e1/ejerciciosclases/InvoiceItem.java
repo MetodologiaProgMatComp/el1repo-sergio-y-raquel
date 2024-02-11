ee9f42bac8b055d92c98d767794c3229e6d88c4e
sergioruizj <sergio.ruizj@edu.uah.es>
2024-02-11 20:08:50 +0100

    11/02 19:28

---
+++ scr/es/uah/matcomp/mp/e1/ejerciciosclases/InvoiceItem.java
@@ -0,0 +1,50 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class InvoiceItem {
+    String ID;
+    String desc;
+    int qty;
+    double unitPrice;
+    public void invoiceItem(String ID,String desc,int qty,double uP){
+        this.desc=desc;
+        this.ID=ID;
+        this.qty=qty;
+        this.unitPrice=uP;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    public double getTotal(){
+        return unitPrice*qty;
+    }
+
+    public String toString() {
+        return "InvoiceItem{" +
+                "ID='" + ID + '\'' +
+                ", desc='" + desc + '\'' +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
