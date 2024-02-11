78651e5ad8e7ab1a815ed0418a04c0956d7c4297
sergioruizj <sergio.ruizj@edu.uah.es>
2024-02-11 20:26:13 +0100

    11/02 20:26

--- scr/es/uah/matcomp/mp/e1/ejerciciosclases/Account.java
+++ scr/es/uah/matcomp/mp/e1/ejerciciosclases/Account.java
@@ -1,4 +1,66 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases;
 
 public class Account {
+    String Id;
+    String name;
+    int balance = 0;
+
+    public void account(String Id, String name) {
+        this.Id = Id;
+        this.name = name;
+    }
+
+    public void account(String Id, String name, int balance) {
+        this.Id = Id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int credit(int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int debit(int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Amount exeeded balance");
+        }
+        return balance;
+    }
+
+    public int transferTo(Account another,int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            another.setBalance(amount);
+        } else {
+            System.out.println("Amount exceeded balance");
+        }
+        return balance;
+    }
+
+    public String toString() {
+        return "Account{" +
+                "Id='" + Id + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
