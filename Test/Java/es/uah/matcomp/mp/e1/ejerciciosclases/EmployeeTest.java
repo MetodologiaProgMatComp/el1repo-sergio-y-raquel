26fc7dd099f722212ef38df4185d7d584d12c5da
raquelvegas <raquel.vegas@edu.uah.es>
2024-02-15 21:06:14 +0100

    Push

--- Test/Java/es/uah/matcomp/mp/e1/ejerciciosclases/EmployeeTest.java
+++ Test/Java/es/uah/matcomp/mp/e1/ejerciciosclases/EmployeeTest.java
@@ -1,6 +1,7 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases;
 
 import es.uah.matcomp.mp.e1.ejerciciosclases.P1.Employee;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
