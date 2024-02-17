c7c7c2ae2e49b451ef6d716029bbd43631171390
raquelvegas <raquel.vegas@edu.uah.es>
2024-02-17 13:03:32 +0100

    2.7 actividad (falta test y main)

---
+++ scr/es/uah/matcomp/mp/e1/ejerciciosclases/P2/e2_7/MyPoint.java
@@ -0,0 +1,65 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P2.e2_7;
+
+public class MyPoint {
+    private int x;
+    private int y;
+
+    ////////////////////////////////////////////////////////////////
+    public MyPoint() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    ////////////////////////////////////////////////////////////////
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        int[] ints = new int[]{x,y};
+        return ints;
+    }
+
+    public void setXY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return String.format("(%s,%s)",x,y);
+    }
+
+    public double distance(int x, int y) {
+        int deltaX = this.x - x;
+        int deltaY = this.y - y;
+        return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+    }
+
+    public double distance(MyPoint another) {
+        int deltaX = this.x - another.x;
+        int deltaY = this.y - another.y;
+        return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+    }
+
+    public double distance() {
+        return Math.sqrt(x*x + y*y);
+    }
+}
