2de668ca0c4630ae14f82bd38ee46d71e0833eb5
raquelvegas <raquel.vegas@edu.uah.es>
2024-02-17 19:14:29 +0100

    2.7 main (falta test)

---
+++ scr/es/uah/matcomp/mp/e1/ejerciciosclases/P2/e2_7/TestMain.java
@@ -0,0 +1,38 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P2.e2_7;
+
+public class TestMain {
+    public static void main(String[] args) {
+        MyLine l1 = new MyLine(1,2,3,4);
+        System.out.println(l1+"\n"+
+                l1.getBegin()+ "\n"+
+                l1.getEnd()+ "\n"+
+                l1.getBeginX()+ "\n"+
+                l1.getBeginY()+"\n"+
+                l1.getEndX()+"\n"+
+                l1.getEndY()+"\n"+
+                l1.getBeginXY()+"\n"+
+                l1.getEndXY()+"\n");
+
+        l1.setBegin(new MyPoint(5,6));
+        l1.setEnd(new MyPoint(7,8));
+        System.out.println(l1.getBegin()+ "\n"+
+                l1.getEnd()+ "\n");
+
+        l1.setBeginX(9);
+        l1.setBeginY(10);
+        l1.setEndX(11);
+        l1.setEndY(12);
+        System.out.println(l1.getBegin()+ "\n"+
+                l1.getEnd()+ "\n");
+
+        l1.setBeginXY(13,14);
+        l1.setEndXY(15,16);
+        System.out.println(l1.getBegin()+ "\n"+
+                l1.getEnd()+ "\n");
+
+        MyLine l2 = new MyLine(new MyPoint(4,0),new MyPoint(4,4));
+        System.out.println(l2);
+        System.out.println(l2.getLength());
+        System.out.println(l2.getGradient());
+    }
+}
---
+++ scr/es/uah/matcomp/mp/e1/ejerciciosclases/P2/e2_8/TestMain.java
@@ -0,0 +1,4 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P2.e2_8;
+
+public class TestMain {
+}
