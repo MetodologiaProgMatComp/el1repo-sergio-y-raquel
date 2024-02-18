9f97ac12fa651d00148bab300317974cda3cfde9
sergioruizj <sergio.ruizj@edu.uah.es>
2024-02-18 02:07:01 +0100

    e2_2. Acabado AuthorTest.java, falta acabar BookTest.java (no se hacerlo)

---
+++ Test/Java/es/uah/matcomp/mp/e1/ejerciciosclases/P2/e2_2/BookTest.java
@@ -0,0 +1,40 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P2.e2_2;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+    Author[] authors = new Author[3];
+    authors[0]=new Author("Kafka","kafka@email",'m');
+    authors[1]=new Author("Cervantes","cervantes@hotmail",'m');
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getQty() {
+    }
+
+    @Test
+    void setQty() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getAuthorNames() {
+    }
+}
