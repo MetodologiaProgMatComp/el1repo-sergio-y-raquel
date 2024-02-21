03407392b605a50ddc264be17accdec7f0ca711c
raquelvegas <raquel.vegas@edu.uah.es>
2024-02-21 08:26:01 +0100

    Tests 21.02

---
+++ Test/Java/es/uah/matcomp/mp/e1/ejerciciosclases/P2/e2_3/AuthorTest.java
@@ -0,0 +1,31 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P2.e2_3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorTest {
+    Author a1=new Author("Juan","juan@email");
+    @Test
+    void getName() {
+        assertEquals(a1.getName(),"Juan");
+    }
+
+    @Test
+    void getEmail() {
+        assertEquals(a1.getEmail(),"juan@email");
+    }
+
+    @Test
+    void setEmail() {
+        a1.setEmail("juan@hotmail");
+        assertEquals(a1.getEmail(),"juan@hotmail");
+    }
+
+
+    @Test
+    void testToString() {
+        Author a2 =new Author("Luisa","luisa@hotmail");
+        assertEquals(a2.toString(),"Author[name=Luisa, email=luisa@hotmail]");
+    }
+}
---
+++ Test/Java/es/uah/matcomp/mp/e1/ejerciciosclases/P2/e2_4/CustomerTest.java
@@ -0,0 +1,40 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.P2.e2_4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @Test
+    void getId() {
+        Customer c1 = new Customer(1, "Felipe", 20);
+        assertEquals(c1.getName(), "Felipe");
+    }
+
+    @Test
+    void getName() {
+        Customer c1 = new Customer(1, "Felipe", 20);
+        assertEquals(c1.getId(), 1);
+    }
+
+    @Test
+    void getDiscount() {
+        Customer c1 = new Customer(1, "Felipe", 20);
+        assertEquals(c1.getDiscount(), 20);
+    }
+
+    @Test
+    void setDiscount() {
+        Customer c1 = new Customer(1, "Felipe", 20);
+        c1.setDiscount(25);
+        assertEquals(c1.getDiscount(), 25);
+    }
+
+    @Test
+    void testToString() {
+        Customer c1 = new Customer(1, "Felipe", 20);
+        assertEquals(c1, "Felipe(1)(20%)");
+
+    }
+}
